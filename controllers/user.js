4678fbe0b4fa1984c7f0d6b6f5d63b715247aa31
sammou00 <sammoustafa00@gmail.com>
2024-10-16 12:14:22 +0200

    intial commit

---
+++ controllers/user.js
@@ -0,0 +1,5 @@
+import db from '../models/index.js';
+
+const User = db.users;
+
+
---
+++ models/index.js
@@ -0,0 +1,56 @@
+import dotenv from 'dotenv';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import { DataTypes, Sequelize } from 'sequelize';
+
+// import models
+import createUser from './user.js';
+
+// construct path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// path configuration
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        dialectOptions: {
+            socketPath: '/tmp/mysql.sock'
+        },
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    }
+);
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch((err) => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.users = createUser(sequelize, DataTypes);
+
+db.sequelize.sync({ force: false }).then(() => {
+    console.log('yes re-sync done!');
+});
+
+export default db;
